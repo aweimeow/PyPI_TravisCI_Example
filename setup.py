d2ba57f8a1746735195f79d022ff199c538ef5ee
aweimeow <ccps960718456@gmail.com>
2017-03-29 12:41:45 +0800

    add all required file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'PyPI_TravisCI_Example',
+  packages = ['PyPI_TravisCI_Example'],
+  version = '0.1',
+  description = 'This is an example for how to build project onto PyPI & TravisCI',
+  author = 'aweimeow(Wei-You Chen)',
+  author_email = 'aweimemow.tw@gmail.com',
+  url = 'https://github.com/aweimeow/PyPI_TravisCI_Example',
+  download_url = 'https://github.com/aweimeow/PyPI_TravisCI_Example/archive/0.1.tar.gz',
+  keywords = ['PyPI', 'TravisCI', 'example'],
+  classifiers = [],
+)
